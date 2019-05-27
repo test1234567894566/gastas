61c74e78ad62d045cea580d2110ce3c1fb6d2577
test1234567894566 <51078585+test1234567894566@users.noreply.github.com>
2019-05-27 14:13:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 ﻿const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix =`^`
+const prefix =`#`
 client.on('ready', () => {
       console.log(`Logged in as ${client.user.tag}!`);
 });
